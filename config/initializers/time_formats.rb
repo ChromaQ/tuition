af2bb682c987a0a479846a9a5e8e4d4c255c21c1
Sarah Swanson <saswanson@salud.unm.edu>
2021-01-21 18:47:14 -0700

    Created initializer for time formatting consistency, added data to views
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -2,7 +2,7 @@
 # Created to format dates and times consistently throughout the app.
 # Date formats:
 Time::DATE_FORMATS[:date4yr] = "%m/%d/%Y"               # time.to_s(:date4yr)      ---->  01/29/2021
-Time::DATE_FORMATS[:date2yr] = "%m/%d/%Y"               # time.to_s(:date2yr)      ---->  01/29/21
+Time::DATE_FORMATS[:date2yr] = "%m/%d/%y"               # time.to_s(:date2yr)      ---->  01/29/21
 Time::DATE_FORMATS[:fancy] = "%A, %d %b %Y"             # time.to_s(:fancy)        ---->  Friday, 29 Jan 2021
 Time::DATE_FORMATS[:datetime] = "%m/%d/%y %H:%M"        # time.to_s(:datetime)     ---->  01/29/21 00:30
 Time::DATE_FORMATS[:timedate] = "%H:%M %m/%d/%y"        # time.to_s(:timedate)     ---->  00:30 01/29/21
