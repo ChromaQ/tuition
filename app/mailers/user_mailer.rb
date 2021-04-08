fb4ad10b1179ba7685daf1262360c9f1ed5fa224
Sarah Swanson <saswanson@salud.unm.edu>
2021-04-07 18:21:26 -0600

    WIP #46 - reminder to submit supporting documents after course end date
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -24,4 +24,15 @@ class UserMailer < ApplicationMailer
       subject: "Tuition Reimbursement Request Approved for #{@user.displayname}"
     )
   end
+
+  def reminder
+    @user = params[:user]
+    @course = params[:course]
+    @url = 'localhost:3000'
+    mail(
+      to: @user.email,
+      cc: @user.employee.manager&.email,
+      subject: "Tuition Reimbursement Reminder to upload supporting documents for #{@course.course_title}"
+    )
+  end
 end
