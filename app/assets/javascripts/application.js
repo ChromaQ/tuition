c26b1f62914a839ecf3cc8a94652445cd808a286
SASwanson <saswanson@salud.unm.edu>
2021-04-29 19:28:07 -0600

    javascript autocomplete
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,16 +4,17 @@
 // require jquery_ujs <-- disabled here because it's in the head and causes console errors
 //= require jquery-ui
 //= require bootstrap
+//= require js-autocomplete
 //= require turbolinks
 //= require activestorage
 //= require_tree .
 
 
 $(document).on("turbolinks:load",function(){
-  // this will dismiss any and all flash alerts after 3 seconds
+  // this will dismiss any and all flash alerts after 6 seconds
   window.setTimeout(function() {
     $(".alert").fadeTo(1000, 0).slideUp(1000, function(){
       $(this).remove();
     });
-  }, 3000);
+  }, 6000);
 });
