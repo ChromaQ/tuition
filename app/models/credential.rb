e689aaa657a8d6db7919afe97c035950081d4640
SASwanson <saswanson@salud.unm.edu>
2021-06-18 13:59:21 -0600

    Implement #77 Credentials simpleform and making indexes look better
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -21,10 +21,18 @@
 #  fk_rails_...  (degree_id => degrees.id)
 #
 class Credential < ApplicationRecord
+
+  # == Relationships ==================================
   belongs_to :degree
-  has_many :courses
   has_many :goals
 
+  # == Validations ====================================
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :degree_id, presence: true
+  validates :auto_approve, presence: true
+
+  # == Methods ====================================
   def full_info
     "#{name}" ' - ' "#{description}"
   end
