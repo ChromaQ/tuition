b41728458d04936a656235046e77c362486d541f
SASwanson <saswanson@salud.unm.edu>
2021-06-28 14:19:28 -0600

    WIP #17 Fiscal year calculations
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,19 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # fiscal year credit calculations
+  def fiscalyear
+    @fiscalyear ||= calculate_fiscalyear
+  end
+
+  # To calculate the current fiscal year - uses HR Datashare_Hybrid calculation
+  def calculate_fiscalyear
+    time = Time.current
+    if time.month >= 7
+      (time.year.to_s[2, 2] + (time + 1.year).year.to_s[2, 2]).freeze
+    else
+      ((time - 1.year).year.to_s[2, 2] + time.year.to_s[2, 2]).freeze
+    end
+  end
 end
