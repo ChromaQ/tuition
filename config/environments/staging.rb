1bd0b6ae96e4adcba4286f7ca2229ff4a810253a
Sarah Swanson <saswanson@salud.unm.edu>
2021-07-12 15:06:16 -0600

    Fixes #89 - bug with mailers on staging. Probably.
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,13 +62,20 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "tuitrem_production"
+  # config.active_job.queue_name_prefix = "tuitrem_staging
 
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
+
+  # default host for staging environment
+  config.action_mailer.default_url_options = { host: 'tuition.staging.health.unm.edu' }
+
+  # Allow Mailer Previews to be accessed on staging
+  config.action_mailer.show_previews = true
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
