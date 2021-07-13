076288ed744635bcbe345ee9dab25f897e5ebfbb
SASwanson <saswanson@salud.unm.edu>
2021-07-12 18:38:12 -0600

    WIP #3 a HR dashboard type page - fix route
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,7 @@ Rails.application.routes.draw do
   get 'impressions', to: 'users#impressions'
   get 'noaccess', to: 'pages#noaccess', as: :noaccess
   get 'template', to: 'pages#template', as: :template
+  get 'hr', to: 'pages#hr', as: :hr
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#welcome'
 end
