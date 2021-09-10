88268c0658b5443b9c78d60be99f59df721d756e
Sarah Swanson <saswanson@salud.unm.edu>
2021-09-10 14:02:57 -0600

    Add mailer preview route to HR page for issue #3
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,39 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'autocomplete/schools', to: 'autocomplete#schools', constraints: { format: %w[js json] }, as: :autocomplete_schools
-  post 'autocomplete/create_school/:school_name', to: 'autocomplete#create_school', as: :create_school_by_name, constraints: { format: %w[js json] }
-
-
-  get 'goals/update_credentials', as: 'update_credentials'
-  resources :goals
-  resources :credentials
-  resources :degrees
-  devise_for :users
-  resources :users
-  resources :courses
-  resources :approvals
-  resources :proofs
+  root to: 'pages#welcome'
   get 'pages/welcome'
-  get 'pages/hr'
-  get 'pages/noaccess'
-  get 'pages/template'
-  get 'users/index'
-  get 'profile' => 'users#show'
-
-  post   'users/impersonation',      to: 'users#impersonate',        as: 'user_impersonation'
-  post   'users/stop_impersonating', to: 'users#stop_impersonating', as: 'stop_impersonating_users'
-  get 'courses/:id/submit', to: 'courses#submit', as: 'submit'
-  post 'courses/:id/approve', to: 'courses#approve', as: 'approve'
-  patch 'courses/:id/withdraw', to: 'courses#withdraw', as: 'withdraw'
-  get 'goals/:id/submit', to: 'goals#submit', as: 'submit_goal'
-  post 'goals/:id/approve', to: 'goals#approve', as: 'approve_goal'
-  get 'proofs/:id/submit', to: 'proofs#submit', as: 'submit_proof'
-  post 'proofs/:id/approve', to: 'proofs#approve', as: 'approve_proof'
-  get 'impressions', to: 'users#impressions'
+  get '/rails/mailers/user_mailer', as: :emails
   get 'noaccess', to: 'pages#noaccess', as: :noaccess
   get 'template', to: 'pages#template', as: :template
   get 'hr', to: 'pages#hr', as: :hr
+
+  resources :credentials
+  resources :degrees
+  resources :approvals
+
+  resources :goals
+  get 'goals/update_credentials', as: 'update_credentials'
+  get 'goals/:id/submit', to: 'goals#submit', as: 'submit_goal'
+  post 'goals/:id/approve', to: 'goals#approve', as: 'approve_goal'
+
+  get 'autocomplete/schools', to: 'autocomplete#schools', constraints: { format: %w[js json] }, as: :autocomplete_schools
+  post 'autocomplete/create_school/:school_name', to: 'autocomplete#create_school', as: :create_school_by_name, constraints: { format: %w[js json] }
+
+  resources :courses
+  get 'courses/:id/submit', to: 'courses#submit', as: 'submit'
+  post 'courses/:id/approve', to: 'courses#approve', as: 'approve'
+  patch 'courses/:id/withdraw', to: 'courses#withdraw', as: 'withdraw'
+
+  resources :proofs
+  get 'proofs/:id/submit', to: 'proofs#submit', as: 'submit_proof'
+  post 'proofs/:id/approve', to: 'proofs#approve', as: 'approve_proof'
+
+  devise_for :users
+  resources :users
+  post   'users/impersonation',      to: 'users#impersonate',        as: 'user_impersonation'
+  post   'users/stop_impersonating', to: 'users#stop_impersonating', as: 'stop_impersonating_users'
+  get 'impressions', to: 'users#impressions'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#welcome'
 end
