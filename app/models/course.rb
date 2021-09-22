4f5cc38aa5bae5d69b7f4ce9156990cd9e949e14
Sarah Swanson <saswanson@salud.unm.edu>
2021-09-22 15:38:29 -0600

    Implement #119 flatpickr implementation
    Signed-off-by: Sarah Swanson <saswanson@salud.unm.edu>

--- app/models/course.rb
+++ app/models/course.rb
@@ -39,6 +39,8 @@ class Course < ApplicationRecord
   # == Validations ====================================
   validates :employee_id, presence: true
   validates :goal_id, presence: true
+  validates :start_date, :end_date, presence: true
+  validates :end_date_after_start_date
 
   # == Scopes =========================================
   scope :submitted_by_employee, ->(employee_id) { where(employee_id: employee_id) }
@@ -70,4 +72,15 @@ class Course < ApplicationRecord
       ((time - 1.year).year.to_s[2, 2] + time.year.to_s[2, 2]).freeze
     end
   end
+
+  # == ClassMethods ===================================
+
+  def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, 'Must be after the start date.')
+    end
+  end
+
 end
